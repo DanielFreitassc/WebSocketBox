ecee918bf46ebd3b29a601b158389c9ae30a39b8
DanielFreitassc <danielfreitassc@gmail.com>
2024-03-20 18:31:17 -0300

    att

--- wb/demo/src/main/java/com/example/demo/StatusController.java
+++ wb/demo/src/main/java/com/example/demo/StatusController.java
@@ -1,22 +1,44 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/status")
 public class StatusController {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final StatusRepository statusRepository;
 
-    public StatusController(SimpMessagingTemplate messagingTemplate) {
+    public StatusController(SimpMessagingTemplate messagingTemplate, StatusRepository statusRepository) {
         this.messagingTemplate = messagingTemplate;
+        this.statusRepository = statusRepository;
     }
 
-    @MessageMapping("/status")
-    public void handleStatusUpdate(Status status) {
+    @PutMapping("/{id}")
+    public void handleStatusUpdate(@PathVariable Long id, @RequestBody Status status) {
+        // Verifica se o status com o mesmo id já existe
+        List<Status> statuses = statusRepository.getStatuses();
+        boolean found = false;
+        for (int i = 0; i < statuses.size(); i++) {
+            if (statuses.get(i).getId().equals(id)) {
+                // Atualiza o status existente com os dados fornecidos na solicitação PUT
+                statuses.get(i).setDisponivel(status.isDisponivel());
+                found = true;
+                break;
+            }
+        }
+
+
+        // Envie o status atualizado para os clientes
         messagingTemplate.convertAndSend("/topic/status", status);
+
+        // Salva os status atualizados no repositório
+        statusRepository.saveStatuses(statuses);
     }
-
 }
-
