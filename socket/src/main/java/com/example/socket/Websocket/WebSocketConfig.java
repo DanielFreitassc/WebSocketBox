afd73eb7d656920b37b61d45ff0d0f444262cf45
danielfreitassc <danielfreitassc@gmail.com>
2024-03-24 00:08:15 -0300

    funcionou

--- socket/src/main/java/com/example/socket/Websocket/WebSocketConfig.java
+++ socket/src/main/java/com/example/socket/Websocket/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -18,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS();
     }
 }
+
 
